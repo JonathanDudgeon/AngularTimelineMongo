c5d29bf8b5db0a9531021df0926f75eae30ef37d
Jonathan Dudgeon <Jonathan_Dudgeon@Yahoo.co.uk>
2014-11-15 15:22:07 +0000

    Update to point to my Mongo DB.  Site Working on my VM  now

--- AngularServiceStackMongo/AngularServiceStackMongo.Domain/Data/MongoConnectionHandler.cs
+++ AngularServiceStackMongo/AngularServiceStackMongo.Domain/Data/MongoConnectionHandler.cs
@@ -8,7 +8,7 @@
 
         public MongoConnectionHandler()
         {
-            const string connectionString = "";
+            const string connectionString = "mongodb://jonathan_dudgeon:jonathan_dudgeon@ds053080.mongolab.com:53080/jonsmongodb";
 
             //// Get a thread-safe client object by using a connection string
             var mongoClient = new MongoClient(connectionString);
@@ -18,7 +18,7 @@
 
             //// Get a reference to the "retrogamesweb" database object
             //// from the Mongo server object
-            const string databaseName = "test_app";
+            const string databaseName = "jonsmongodb";
             var db = mongoServer.GetDatabase(databaseName);
 
             //// Get a reference to the collection object from the Mongo database object
